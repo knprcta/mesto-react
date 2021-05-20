81926371339f5f2452e99e4f832f6c76f394d50f
Kirill Fedotov <knprcta@yandex.ru>
2021-05-20 21:12:12 +0300

    feat: Лайки и удаление карточек

--- src/components/Card.js
+++ src/components/Card.js
@@ -18,17 +18,25 @@ function Card(props) {
     props.onCardClick(props.card);
   }
 
+  function handleLikeClick() {
+    props.onCardLike(props.card);
+  }
+
+  function handleDeleteClick() {
+    props.onCardDelete(props.card);
+  }
+
   return (
     <article className="element">
       <div className="element__image" style={{ backgroundImage: `url(${props.card.link})` }} onClick={handleClick}></div>
       <div className="element__bottom">
         <h2 className="element__caption">{props.card.name}</h2>
         <div className="element__like-container">
-          <button className={cardLikeButtonClassName} type="button"></button>
+          <button className={cardLikeButtonClassName} type="button" onClick={handleLikeClick}></button>
           <span className="element__likes-count">{props.card.likes.length}</span>
         </div>
       </div>
-      <button className={cardDeleteButtonClassName} type="button"></button>
+      <button className={cardDeleteButtonClassName} type="button" onClick={handleDeleteClick}></button>
     </article>
   )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -15,7 +15,23 @@ function Main(props) {
       .catch(err => {
         console.log(err);
       })
-  }, [])
+  }, []);
+
+  function handleCardLike(card) {
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    
+    api.toogleLike(card._id, isLiked)
+      .then((newCard) => {
+        setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
+      });
+  }
+  
+  function handleCardDelete(card) {
+    api.deleteCard(card._id)
+      .then(() => {
+        setCards((state) => state.filter((c) => c._id !== card._id));
+      })
+  }
 
   return (
     <main>
@@ -38,7 +54,7 @@ function Main(props) {
 
       <section className="elements">
         {cards.map(item => (
-          <Card key={item._id} card={item} onCardClick={props.onCardClick} />
+          <Card key={item._id} card={item} onCardClick={props.onCardClick} onCardLike={handleCardLike} onCardDelete={handleCardDelete} />
         ))}
       </section>
     </main>
